dc8b40b1b3178382977d31c3930676c9adb3e4ab
mddalam1 <mddalam1@gmail.com>
2020-12-17 11:00:35 +0530

    first commit

---
+++ Database/Queries.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE cei;
+
+CREATE TABLE users(
+	id SERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	password TEXT NOT NULL,
+	role VARCHAR(255) DEFAULT 'user'
+);
+
+INSERT INTO users VALUES (DEFAULT, 'Md Dilshad Alam', 'mddalam1@gmail.com', 'uyh43gy7ghbt6dt3vxgvc536gcyvcyttfc7tcvv', 'admin');
+
